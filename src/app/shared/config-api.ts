838a02c73e37531b3103168f337baa1e85cbe004
thomas-bressel <tbressel@outlook.fr>
2025-12-04 02:04:34 +0100

    fix: change fetch adress api for auth

--- src/app/shared/config-api.ts
+++ src/app/shared/config-api.ts
@@ -1,12 +1,12 @@
 const userApiEnv = {
     local: 'http://localhost:5002',
     production: 'https://api-user.asmtariste.fr:5002',
-    staging: 'https://staging.asmtariste.fr/api/user',
+    staging: 'https://staging.drawer.auroreia.fr/api/user',
 }
 const contentApiEnv = {
     local: 'http://localhost:5001',
     production: 'https://api-content.asmtariste.fr:5001',
-    staging: 'https://staging.asmtariste.fr/api/content',
+    staging: 'https://staging.drawer.auroreia.fr/api/content',
 }
 
 export const USER_API_URI: string = userApiEnv.staging;
