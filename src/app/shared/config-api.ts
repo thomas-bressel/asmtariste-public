2479f6b7c3baa5292956c19714b9dd01e3d95029
thomas-bressel <tbressel@outlook.fr>
2025-12-04 01:50:40 +0100

    fix: change fetch adress api to staging

--- src/app/shared/config-api.ts
+++ src/app/shared/config-api.ts
@@ -1,11 +1,13 @@
 const userApiEnv = {
     local: 'http://localhost:5002',
     production: 'https://api-user.asmtariste.fr:5002',
+    staging: 'https://staging.asmtariste.fr/api/user',
 }
 const contentApiEnv = {
     local: 'http://localhost:5001',
-    production: 'https://api-content.asmtariste.fr:5001'
+    production: 'https://api-content.asmtariste.fr:5001',
+    staging: 'https://staging.asmtariste.fr/api/content',
 }
 
-export const USER_API_URI: string = userApiEnv.local;
-export const CONTENT_API_URI: string = contentApiEnv.local;
+export const USER_API_URI: string = userApiEnv.staging;
+export const CONTENT_API_URI: string = contentApiEnv.staging;
