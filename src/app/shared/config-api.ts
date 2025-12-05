be7171caad9f86cbe010854d696ff1e1fddd8343
thomas-bressel <tbressel@outlook.fr>
2025-12-05 18:47:21 +0100

    fix: adapting  Dockerfil.prod and workflow production file

--- src/app/shared/config-api.ts
+++ src/app/shared/config-api.ts
@@ -1,13 +1,13 @@
 const userApiEnv = {
     local: 'http://localhost:5002',
-    production: 'https://api-user.asmtariste.fr:5002',
+    production: 'https://staging.drawer.auroreia.fr/api/user',
     staging: 'https://staging.drawer.auroreia.fr/api/user',
 }
 const contentApiEnv = {
     local: 'http://localhost:5001',
-    production: 'https://api-content.asmtariste.fr:5001',
+    production: 'https://staging.drawer.auroreia.fr/api/content',
     staging: 'https://staging.drawer.auroreia.fr/api/content',
 }
 
-export const USER_API_URI: string = userApiEnv.staging;
-export const CONTENT_API_URI: string = contentApiEnv.staging;
+export const USER_API_URI: string = userApiEnv.production;
+export const CONTENT_API_URI: string = contentApiEnv.production;
